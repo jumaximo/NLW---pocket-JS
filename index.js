7ffc815e6e9ddfaf1d9892dc87c43621e3e98393
Julia <julia.maximo08@gmail.com>
2024-09-18 16:25:32 -0300

    Metas abertas

--- index.js
+++ index.js
@@ -56,11 +56,26 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
     })
 }
 
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true 
+    })
+
+    if(abertas.length == 0){
+        console.log('Não existem metas abertas :)')
+        return
+    }
+
+    await select ({
+        message: 'Metas abertas ' + abertas.length,
+        choices: [...abertas]
+    })
+}
     
 
 const start = async () => {
@@ -83,6 +98,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -99,6 +118,9 @@ const start = async () => {
                 break 
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima")
